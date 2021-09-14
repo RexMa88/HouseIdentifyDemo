8834cc72582fd5057a4f24ec3cdae13b7edc47c4
machao13 <machao13@longfor.com>
2021-09-14 16:09:30 +0800

    Initial Commit

---
+++ HouseIdentifyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HouseIdentifyDemo
+//
+//  Created by machao on 2021/9/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
