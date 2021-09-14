b88111b13821ac9622cc7d67e7d737120e27a2d0
machao13 <machao13@longfor.com>
2021-09-14 17:05:05 +0800

    add: Gallery && Basic UI

--- HouseIdentifyDemo/ViewController.swift
+++ HouseIdentifyDemo/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import Gallery
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var galleryBtn: UIButton!
+    @IBOutlet weak var photosBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        galleryBtn.layer.cornerRadius = 20.0
+        
+        photosBtn.layer.cornerRadius = 20.0
+        
+        Config.Font.Text.bold = UIFont.systemFont(ofSize: 14.0, weight: .semibold)
+        Config.Camera.recordLocation = true
+        Config.tabsToShow = [.imageTab, .cameraTab]
     }
+    
+    @IBAction func galleryBtnAction(_ sender: Any) {
+        
+        let gallery = GalleryController()
+        gallery.delegate = self
+        
+        present(gallery, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func getPhotosBtnAction(_ sender: Any) {
+        
+    }
+    
+}
 
-
+// MARK: - GalleryControllerDelegate
+extension ViewController: GalleryControllerDelegate {
+    
+    func galleryController(_ controller: GalleryController, didSelectVideo video: Video) {
+        
+    }
+    
+    func galleryControllerDidCancel(_ controller: GalleryController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func galleryController(_ controller: GalleryController, didSelectImages images: [Image]) {
+        
+    }
+    
+    func galleryController(_ controller: GalleryController, requestLightbox images: [Image]) {
+        
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-HouseIdentifyDemo/Pods-HouseIdentifyDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HouseIdentifyDemo : NSObject
+@end
+@implementation PodsDummy_Pods_HouseIdentifyDemo
+@end
